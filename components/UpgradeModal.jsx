7a4ac0c0403b5d502df41b1bb42ee649f5614033
sayan629 <sayanpal771@gmail.com>
2026-05-31 00:36:52 +0530

    Role based Navigation part is done..

--- components/UpgradeModal.jsx
+++ components/UpgradeModal.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client";
 
-const UpgradeModal = ({ open, onOpenChange , reason}) => {
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import PricingSection from "./PricingSection";
+import { AlertCircle } from "lucide-react";
+
+export default function UpgradeModal({ open, onOpenChange, reason }) {
   return (
-    <div>UpgradeModal</div>
-  )
-}
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="border-amber-200/10 min-w-[70vw] max-h-[90vh] overflow-y-scroll">
+        <DialogHeader>
+          <div className="flex items-start gap-2 mb-2">
+            <AlertCircle className="text-amber-400 ml-2 mt-1" />
+            <div>
+              <DialogTitle className="font-serif text-2xl">
+                Upgrade your plan
+              </DialogTitle>
+              {reason && (
+                <DialogDescription className="text-amber-400 mt-1">
+                  {reason}
+                </DialogDescription>
+              )}
+            </div>
+          </div>
+        </DialogHeader>
 
-export default UpgradeModal
+        {/* PricingSection or any children slot in here */}
+        <div className="px-2 pb-6">
+          <PricingSection />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
