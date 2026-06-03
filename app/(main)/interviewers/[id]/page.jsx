8670153e6c6b48fd4e5dd34acfd4d18ce7bff29c
sayan629 <sayanpal771@gmail.com>
2026-06-04 02:58:07 +0530

    Booking page..

--- app/(main)/interviewers/[id]/page.jsx
+++ app/(main)/interviewers/[id]/page.jsx
@@ -1,9 +1,32 @@
+import { getInterviewerProfile } from '@/actions/booking';
+import { Button } from '@/components/animate-ui/primitives/buttons/button';
+import { StarsBackgroundDemo } from '@/components/demo-components-backgrounds-stars';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const InterviewerProfilePage = () => {
-  return (
-    <div>InterviewerProfilePage</div>
-  )
+const InterviewerProfilePage = async({ params }) => {
+  const { id } = await params;
+
+  const interviewer= await getInterviewerProfile(id);
+  if(!interviewer) notFound();
+
+
+  return <main className='min-h-screen bg-black'>
+    <section className="relative border-b border-white/8 overflow-hidden">
+    <StarsBackgroundDemo/>
+    
+    <div className="relative max-w-6xl mx-auto px-8 pt-20 pb-14 flex flex-col gap-8">
+      <Link href="/explore">
+        <Button variant = "link" className="text-stone-500 cursor-pointer">
+          <ArrowLeft size={13}/>
+          Back to explore
+        </Button>
+      </Link>
+    </div>
+    </section>
+  </main>
 }
 
 export default InterviewerProfilePage
