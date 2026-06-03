6e1528bd2ceff6462838a2e6ed668726c34d089d
sayan629 <sayanpal771@gmail.com>
2026-06-04 02:47:10 +0530

    Add Shield and bot protection.

--- proxy.js
+++ proxy.js
@@ -1,3 +1,4 @@
+import arcjet, { detectBot, shield } from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
@@ -8,8 +9,28 @@ const isProtectedRoute = createRouteMatcher([
   "/onboarding(.*)",
 ]);
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({mode:"LIVE"}),
+
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE","CATEGORY:PREVIEW"],
+    })
+  ],
+});
+
 export default clerkMiddleware(async(auth, req)=>{
+
+    // Apply Arcjet protection FIRST(before Clerk auth check)
+      const decision = await aj.protect(req);
+      if (decision.isDenied()) {
+        return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+      }
+    
     const { userId } = await auth();
+
   if(!userId && isProtectedRoute(req)){
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
