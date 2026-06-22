aaac7acde8ea97762caaf9e648d38de21ee0ac25
sayan629 <sayanpal771@gmail.com>
2026-06-22 12:31:07 +0530

    Call Room UI.

--- app/(main)/call/[callId]/_components/CallUI.jsx
+++ app/(main)/call/[callId]/_components/CallUI.jsx
@@ -2,6 +2,8 @@
 
 import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
+import { useEffect, useState } from "react";
+import { useCreateChatClient } from "stream-chat-react";
 import "stream-chat-react/dist/css/v2/index.css";
 
 
@@ -21,5 +23,40 @@ export default function CallUI({
     const call = useCall();
     const callingState = useCallCallingState();
 
-    
+    const [activeTab, setActiveTab] = useState("chat");
+
+    const chatClient = useCreateChatClient({
+        apiKey,
+        tokenOrProvider:token,
+        userData: {
+            id: currentUser.id,
+            name: currentUser.name,
+            image: currentUser.imageUrl,
+        },
+    });
+
+    const[chatChannel, setChatChannel] = useState(null);
+
+    useEffect (() => {
+        if (!chatClient) return;
+
+        const channel = chatClient.channel("messaging", callId, {
+            name: "Interview Chat",
+            members: [
+                booking.interviewer.clerkUserId,
+                booking.interviewee.clerkUserId,
+            ],
+        });
+
+        channel
+            .watch()
+            .then(() => setChatChannel(channel))
+            .catch(console.error);
+
+            return () => {
+                channel.stopWatching().catch(() => {});
+            };
+    },[chatClient, callId, booking]);
+
+
 }
