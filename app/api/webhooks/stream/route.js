24554b2763d8ee0af416d8d5323415f0b2eff255
sayan629 <sayanpal771@gmail.com>
2026-06-25 01:04:36 +0530

    Recording and Feedback webhook...

--- app/api/webhooks/stream/route.js
+++ app/api/webhooks/stream/route.js
@@ -4,6 +4,12 @@ export async function POST(request) {
     const body = await request.json();
     const eventType = body.type;
 
-    return Response.json({ message: "Subscribe" })
-    
+    if (
+        eventType !== "call.transcription_ready" &&
+        eventType !== "call.recording_ready"
+    ){
+    return Response.json({ ok: true })
+    }
+    const callCid = body.call_cid ?? "";
+    const streamCallId = callCid.includes(":") ? callCid.split(":")[1] : callCid;
 }
