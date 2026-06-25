0f60799d24d3cdf5b9816e5aeaf07363bae0b531
sayan629 <sayanpal771@gmail.com>
2026-06-25 10:02:22 +0530

    Interviewer Dashboard

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -9,4 +9,16 @@ export const setAvailability = async ({ startTime, endTime }) => {
 
     const dbUser = await db.user.findUnique({ where: { clerkUserId: user.id }});
     if(!dbUser || dbUser.role !== "INTERVIEWER") throw new Error("Forbidden");
+
+    if (!startTime || !endTime ) throw new Error("Start and end time required");
+    if (new Date(startTime) >= new Date(endTime))
+        throw new Error("Start time must be before end time");
+
+    try {
+        const existing = await db.availability.findFirst({
+            where: { interviewerId: dbUser.id, status:"AVAILABLE"},
+        });
+    } catch (error) {
+
+    }
 }
